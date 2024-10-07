5315ce776a0d51ede8988000415c5ec6cfeb1dad
Amira Tarek <amirat737@gmail.com>
2024-10-08 01:55:47 +0300

    build home offer widget

--- lib/features/home/presentation/views/widgets/home_offers_widget.dart
+++ lib/features/home/presentation/views/widgets/home_offers_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:store/core/utils/constants.dart';
+import 'package:store/core/utils/color_palette.dart';
+import 'package:store/core/utils/styles.dart';
 
 class HomeOffersWidget extends StatelessWidget {
   const HomeOffersWidget({super.key});
@@ -13,19 +14,51 @@ class HomeOffersWidget extends StatelessWidget {
           children: [
             Text(
               'Special Offers',
-              style: TextStyle(
-                fontSize: 20,
-                fontFamily: Constants().kUrwBold,
-              ),
+              style: Styles.style20Bold,
             ),
             Text(
               'See All',
-              style: TextStyle(
-                fontSize: 16,
-                fontFamily: Constants().kUrwBold,
-              ),
+              style: Styles.style16Bold,
             ),
           ],
+        ),
+        const SizedBox(height: 20),
+        Container(
+          padding: const EdgeInsets.only(left: 25, top: 25, bottom: 25),
+          height: 136,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: ColorPalette.kBlueLogo,
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+          ),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '25%',
+                    style: Styles.style36Bold,
+                  ),
+                  Text(
+                    "Today's Special!",
+                    style: Styles.style20Bold.copyWith(
+                      color: ColorPalette.kWhite,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Flexible(
+                flex: 2,
+                child: Image.asset(
+                  'assets/pics/shoes.png',
+                  width: 180,
+                  height: 180,
+                ),
+              )
+            ],
+          ),
         ),
       ],
     );
